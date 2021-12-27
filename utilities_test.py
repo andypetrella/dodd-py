0827d1aad288df8b6e7c141c2a7da34df4668666
Andy Petrella <andy.petrella@kensu.io>
2021-12-27 10:39:47 +0100

    only pass

--- utilities_test.py
+++ utilities_test.py
@@ -6,12 +6,7 @@ def test_uuid():
     uuid = utilities.create_uuid()
     assert len(uuid) == 32
     assert uuid.find('-') == -1
- 
-# Sample of a failing test
-def test_failing():
-    uuid = utilities.create_uuid()
     if os.environ["environment"] in ["Development", "Production"]:
         assert os.environ["KENSU_TOKEN"] is None
     else:
         assert os.environ["KENSU_TOKEN"] is ''
-    assert uuid is None
