add75e4e9e34cedfab17914adf97e00364449699
Andy Petrella <andy.petrella@kensu.io>
2021-12-27 10:32:08 +0100

    check on secrets

--- utilities_test.py
+++ utilities_test.py
@@ -4,12 +4,11 @@ import utilities
 # Sample of a working test
 def test_uuid():
     uuid = utilities.create_uuid()
-    print(os.environ["KENSU_TOKEN"])
     assert len(uuid) == 32
     assert uuid.find('-') == -1
  
 # Sample of a failing test
 def test_failing():
-    print(os.environ["KENSU_TOKEN"])
     uuid = utilities.create_uuid()
+    assert os.environ["KENSU_TOKEN"] is None
     assert uuid is None
