a582ead2018859d7fab0bfb20b765e44dbf4516f
Andy Petrella <andy.petrella@kensu.io>
2022-01-01 20:09:14 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import find_packages, setup
+from os.path import splitext
+from os.path import basename
+from glob import glob
 
 setup(
     name="dodd-py", 
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    
+
 )
